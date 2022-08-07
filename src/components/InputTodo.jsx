16f38961fda2741a301769ca5bea3a7db9d45fdc
ShunOnuma1025 <62339890+ShunOnuma1025@users.noreply.github.com>
2022-08-07 17:49:44 +0900

    CSS-in-JS

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 
+const style = {
+  backgroundColor: "#c1ffff",
+  width: "400px",
+  height: "30px",
+  borderRadius: "8px",
+  padding: "8px",
+  margin: "8px"
+};
+// このスタイルの書き方が正しいというわけではないが、こういう記述もある
+
 export const InputTodo = (props) => {
   const { todoText, onChange, onClick } = props;
   return (
-    <div className="input-area">
+    <div style={style}>
       <input placeholder="TODOを入力" value={todoText} onChange={onChange} />
       {/* inputの値を取り出したいので、入力したvalueをStateとして定義した */}
       <button onClick={onClick}>追加</button>
